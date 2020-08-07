23a6d9d835a422ae44aa8181443a7aca880bf76d
Hayden Eastwell <42878077+haydende@users.noreply.github.com>
2020-08-07 08:51:29 +0100

    Added DataLoader script to get some initial data

--- src/main/java/haydende/springmvcrest/bootstrap/DataLoader.java
+++ src/main/java/haydende/springmvcrest/bootstrap/DataLoader.java
@@ -1,15 +1,27 @@
 package haydende.springmvcrest.bootstrap;
 
+import haydende.springmvcrest.domain.Category;
+import haydende.springmvcrest.repositories.CategoryRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DataLoader implements CommandLineRunner {
 
+    CategoryRepository categoryRepository;
 
+    public DataLoader(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
 
     @Override
     public void run(String... args) throws Exception {
+        Category fruit = new Category();
+        Category nuts = new Category();
+        Category dried = new Category();
 
+        categoryRepository.save(fruit);
+        categoryRepository.save(nuts);
+        categoryRepository.save(dried);
     }
 }
